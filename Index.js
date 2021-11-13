bce160dd60a124f5ca204d9623cc8aa5aa87f568
ADSI-TARDE <ADSI-TARDE@PALDFPCBIFSD247.FormacionSena.RED>
2021-11-13 14:35:56 -0500

    Mi primera conexion

---
+++ Index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.set( 'port' , 5000)
+
+
+
+//routes
+app.get('/', (reg,res)=>{
+
+    res.send('Bienvenidos')
+})
+
+app.listen(app.get('port'), () =>{
+    console.log(`Bienvenido a la aplicacion de node puerto  ${app.get('port')}`)
+})
